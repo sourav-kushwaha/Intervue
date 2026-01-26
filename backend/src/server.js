8bd7ed2af9652dc9ced2eb9c3c7d740e65820f84
Sourav Singh Kushwaha <souravsinghkushwaha20@gmail.com>
2026-01-27 01:11:55 +0530

    deployment config added.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { ENV } from "./lib/env.js";
+
 
 dotenv.config();
 
 
 const app = express();
+const __dirname = path.resolve();
 
 app.get("/sourav",(req,res)=>{
     res.status(200).json({msg:"success from api"})
 });
+app.get("/mummy",(req,res)=>{
+    res.status(200).json({msg:"success from api"})
+});
 
 const PORT = process.env.PORT;
-app.listen(PORT, ()=>{
-    console.log("server is running on port " + PORT);
+
+//making our app ready for the deployment
+if(ENV.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("/{*any}", (req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist", "index.html"))
+    });
+}
+app.listen(ENV.PORT, ()=>{
+    console.log("server is running on port " + ENV.PORT);
 })
 

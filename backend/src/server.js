71ca290147046f75ac580df5294908a708586509
Sourav Singh Kushwaha <souravsinghkushwaha20@gmail.com>
2026-02-21 17:53:25 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT;
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/{*any}", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }

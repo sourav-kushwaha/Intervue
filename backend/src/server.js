b32397793cc92828d04f92dc5d38fbc8d8f1942e
Sourav Singh Kushwaha <souravsinghkushwaha20@gmail.com>
2026-02-24 21:57:40 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
 import { functions, inngest } from "./lib/inngest.js";
-import {serve} from "inngest/express";
+import { serve } from "inngest/express";
 
 import cors from "cors";
 dotenv.config();
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}));
-app.use("/api/inngest", serve({ client: inngest, functions}))
+app.use("/api/inngest", serve(inngest))
 
 app.get("/sourav",(req,res)=>{
     res.status(200).json({msg:"success from api"})

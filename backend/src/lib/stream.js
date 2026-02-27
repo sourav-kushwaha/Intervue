26467b098cb882ffeb64f403155930b10fc180f1
Sourav Singh Kushwaha <souravsinghkushwaha20@gmail.com>
2026-02-27 13:46:11 +0530

    session routes and controllers are added

--- backend/src/lib/stream.js
+++ backend/src/lib/stream.js
@@ -1,5 +1,6 @@
 import {StreamChat} from "stream-chat";
 import {ENV} from "./env.js";
+import {StreamClient} from "@stream-io/node-sdk";
 const apiKey = ENV.STREAM_API_KEY
 const apiSecret = ENV.STREAM_API_SECRET
 
@@ -7,6 +8,7 @@ if(!apiKey || !apiSecret){
     console.error("apikeys missing")
 }
 export const chatClient = StreamChat.getInstance(apiKey,apiSecret);
+export const streamClient = new StreamClient(apiKey,apiSecret);
 export const createStreamUser = async(userData) =>{
     try{
         await chatClient.upsertUser(userData)
